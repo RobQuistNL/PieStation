0c925e053c401049081774a813db743b766a9adf
RobQuistNL <rob.quist@dukesoft.nl>
2015-03-27 13:51:25 +0100

    Add youtube stream functionality

--- public/js/local.js
+++ public/js/local.js
@@ -21,9 +21,40 @@ $.getJSON( "/config/local-config.json", function( data ) {
 });
 
 socket.on('localevent', function(data){
+    console.log(data);
     switch (data.action) {
         case 'refresh':
             window.location = window.location;
             break;
+        case 'youtube':
+            switch (data.do) {
+                case 'newvideo':
+                    playYoutubeVideo(data.video);
+                    break;
+                case 'pausevideo':
+                    pauseYoutubeVideo();
+                    break;
+                case 'resumevideo':
+                    resumeYoutubeVideo();
+                    break;
+                case 'stopvideo':
+                    stopYoutubeVideo();
+                    break;
+            }
+            break;
     }
 });
+
+function playYoutubeVideo(videoId) {
+    if ($('.fullscreen').length == 0) {
+        $('body').append('<div class="fullscreen"></div>');
+    }
+
+    $('.fullscreen').html(
+        "<iframe width='100%' height='100%' src='http://www.youtube.com/embed/"+videoId+"?autoplay=1&controls=0&modestbranding=1&rel=0&theme=dark&iv_load_policy=3&vq=hd1080' frameborder='0' type='text/html'></iframe>"
+    );
+}
+
+function stopYoutubeVideo() {
+    $('.fullscreen').remove();
+}
