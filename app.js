0568a4fe66182f70dade84bd6d1506051edced19
Rob Quist <rob.quist@dukesoft.nl>
2016-04-01 16:33:17 +0200

    Update a lot of changes

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ var isRecording = false;
 eval(fs.readFileSync('./dist/voicecommands.js')+'');
 var request = require('request');
 
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+//var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+//var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+//var credentials = {key: privateKey, cert: certificate};
 
 var http = require('http').Server(app);
-var https = require('https').createServer(credentials, app);
+//var https = require('https').createServer(credentials, app);
 
 var io = require('socket.io')(http);
 var lastLedKey = "unknown";
@@ -32,7 +32,38 @@ var isPi = false;
 
 //Config
 WEBSERVER_PORT = 80; //Listening port
+TTS_VOICE = 'Graham';
+TTS_LANG = 9;
+/*
+UK: 9 -> Rachel
+Graham
+Lucy
 
+US:
+ Sharon
+ Ella (genuine child voice)
+ EmilioEnglish (genuine child voice)
+ Josh (genuine child voice)
+ Karen
+ Kenny (artificial child voice)
+ Laura
+ Micah <- Good male
+ Nelly (artificial child voice)
+ Rod
+ Ryan
+ Saul
+ Scott (genuine teenager voice)
+ Tracy <- Ok
+ ValeriaEnglish (genuine child voice)
+ Will
+ WillBadGuy (emotive voice) <- Cool
+ WillFromAfar (emotive voice)
+ WillHappy (emotive voice) <- Ok
+ WillLittleCreature (emotive voice)
+ WillOldMan (emotive voice)
+ WillSad (emotive voice)
+ WillUpClose (emotive voice) <- Whispering, lol
+ */
 exec('export AUDIODEV=hw:1,0; export AUDIODRIVER=alsa;');
 app.use(express.static(__dirname + '/public'));
 
@@ -55,14 +86,21 @@ io.on('connection', function(socket){
 
 app.get('/send/kaku/:letter/:code/:onoff', function(req, res) {
     // KlikAanKlikUit remote power thingies
-    var letter = req.param("letter");
-    var code = req.param("code");
-    var onoff = req.param("onoff");
+    var letter = req.params.letter;
+    var code = req.params.code;
+    var onoff = req.params.onoff;
     KaKu (letter, code, onoff, res);
 });
 
+app.get('/433/:string', function(req, res) {
+    // KlikAanKlikUit remote power thingies
+    var string = req.params.string;
+    do433 (string, res);
+});
+
+
 app.get('/tv/:ircc', function(req, res) {
-    var ircc = req.param("ircc");
+    var ircc = req.param.ircc;
     sonybravia.sendIRCC(ircc);
 });
 
@@ -78,6 +116,7 @@ function textToSpeech(text, lang) {
     }
     text = encodeURIComponent(text);
     console.log('SPEAK: ' + text);
+    text = text + ". ."
 
     var filename = md5(text);
     var playcmd = 'omxplayer'; //ffplay -i
@@ -88,19 +127,43 @@ function textToSpeech(text, lang) {
             exec(playcmd+" ./speech/"+filename+".mp3" + playArgs);
             setTimeout(function(){sonybravia.setVolume(tv_volume);}, text.length*25);
         } else {
-            exec("curl 'http://translate.google.com/translate_tts?&ie=UTF-8&q="+text+"&tl="+lang+"&client=t' -H " +
-                "'Referer: http://translate.google.com/' -H 'User-Agent: stagefright/1.2 (Linux;Android 5.0)' " +
-                "> ./speech/"+filename+".mp3; "+playcmd+" ./speech/"+filename+".mp3" + playArgs);
+            /*exec("curl $(curl --data 'MyLanguages=sonid10&MySelectedVoice="+TTS_VOICE+"&MyTextForTTS=" + text +
+                "&t=1&SendToVaaS=' 'http://www.acapela-group.com/demo-tts/DemoHTML5Form_V2.php' | grep -o" +
+                " \"http.*mp3\")\"> ./speech/"+filename+".mp3; "+playcmd+" ./speech/"+filename+".mp3" + playArgs);
+                */
+
+            exec("curl $(curl --data 'MyLanguages=sonid"+ TTS_LANG +"&MySelectedVoice="+TTS_VOICE+"&MyTextForTTS=" + text + "&t=1&SendToVaaS=' 'http://www.acapela-group.com/demo-tts/DemoHTML5Form_V2.php' | grep -o \"http.*mp3\") > ./speech/"+filename+".mp3; "+playcmd+" ./speech/"+filename+".mp3" + playArgs);
+
             sonybravia.setVolume(0);
             setTimeout(function(){sonybravia.setVolume(tv_volume);}, text.length*50);
         }
     });
 }
 
+
+
 function playSound(file) {
     var playcmd = 'omxplayer'; //ffplay -i
     var playArgs = ''; //' -v quiet -nodisp -autoexit';
     exec(playcmd + " " + file + playArgs);
+}
+
+function do433(string, res) {
+    console.log('Got 433: ' + string);
+    var command="sudo /var/piestation/dist/433/jeroen/433rgb " + string;
+
+    exec(command, function(error, stdout, stderr){
+        if(error) {
+            var msg = 'ERROR:' + error;
+        } else {
+            var msg = "Sent command!" + stdout + stderr;
+        }
+        if (res != undefined) {
+            res.send(msg);
+        } else {
+            console.log(msg);
+        }
+    });
 }
 
 function KaKu(letter, code, onoff, res) {
@@ -188,10 +251,10 @@ http.listen(WEBSERVER_PORT, function(){
     console.log('listening on *:'+WEBSERVER_PORT);
 });
 
-https.listen(443, function(){
+/*https.listen(443, function(){
     console.log('listening on *:443');
 });
-
+*/
 app.get('/start-listening', function(req, res) {
     listenToSpeech(res);
     return res.send('Listening!');
@@ -201,12 +264,26 @@ app.get('/stop-listening', function(req, res) {
     stopListening();
     return res.send('Stop listening!');
 });
-
+var responses = {
+    notUnderstood: [
+        "Sorry, I did not understand you.",
+        "Lol, come again?",
+        "Sorry, what was that?",
+        "Sorry, I didn't understand a word of that.",
+        "uh. Not sure what you ment. Can you repeat that?"
+    ],
+    dontKnowWhatToDo: [
+        "Sorry, I do not know what to do with that. Spank Rob. It is his fault.",
+        "Hmm. No idea what I should do.",
+        "I eh. uhm. Mmm. I don't know what to do now. bye."
+    ]
+};
 
 function stopListening() {
     if (isRecording == false) {
         return;
     }
+    console.log('Stop recording');
     lpcm16.stop();
     isRecording = false;
     sonybravia.setVolume(tv_volume);
@@ -224,7 +301,8 @@ function listenToSpeech(res) {
 
         lpcm16.start({
             verbose: false,
-            recordProgram: 'arecord'
+            recordProgram: 'arecord',
+            cmdArgs: ['-D', 'plughw:1,0']
         }).pipe(request.post({
                 'url'     : 'https://api.wit.ai/speech?client=chromium&lang=en-us&output=json',
                 'headers' : {
@@ -245,20 +323,21 @@ function listenToSpeech(res) {
         setTimeout(function () {
             stopListening();
         }, 5000);
-    }, 900);
+    }, 1900);
 }
 console.log(voicecommands.commands);
 function parseSpeech(res) {
     if (res.outcomes == undefined || res.outcomes.length == 0) {
         //textToSpeech('Lol. Sorry. I did not understand a word of that.');
-        textToSpeech('Nope.');
+
+        textToSpeech(responses.notUnderstood[Math.floor(Math.random() * responses.notUnderstood.length)]);
         console.log('No outcomes.');
         return;
     }
     _.each(res.outcomes, function(outcome) {
         if (outcome.confidence < .50) {
             //textToSpeech('You said ' + outcome._text + '. I am not sure what you ment.');
-            textToSpeech('Nope.');
+            textToSpeech(responses.notUnderstood[Math.floor(Math.random() * responses.notUnderstood.length)]);
             return;
         }
 
@@ -266,7 +345,8 @@ function parseSpeech(res) {
         if (commandFunction != undefined && outcome.entities.length != 0) {
             commandFunction(outcome.entities);
         } else {
-            textToSpeech('Quite sure you said "' + outcome._text + '". But i have no commands assigned to that.');
+            //textToSpeech('Quite sure you said "' + outcome._text + '". But i have no commands assigned to that.');
+            textToSpeech(responses.dontKnowWhatToDo[Math.floor(Math.random() * responses.dontKnowWhatToDo.length)]);
         }
     });
 
