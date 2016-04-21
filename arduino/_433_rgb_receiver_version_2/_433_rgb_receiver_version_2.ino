88fcf176f47ef300cf855b8b12d9d9add148e206
Rob Quist - win7@home <rob.quist@dukesoft.nl>
2016-04-21 22:39:44 +0200

    Add version 2 433 receiver code

--- arduino/_433_rgb_receiver_version_2/_433_rgb_receiver_version_2.ino
+++ arduino/_433_rgb_receiver_version_2/_433_rgb_receiver_version_2.ino
@@ -10,7 +10,7 @@ ADDRESS     UNIT  Effect
             5     Fade 1 - 2
             6     Fade 1 - 2 - 3
             7     Fade 2 - 3
-            8     Fade 2 - 3
+            8     Fade 1 - 3
             9     flash red X times
             10    flash blue X times
             11    flash green X times
@@ -51,96 +51,88 @@ ADDRESS     UNIT  Effect
 
 #include <NewRemoteReceiver.h>
 
-// don't futz with these, illicit sums later
 #define PIN_RED       9// pin for red LED
 #define PIN_GREEN    10 // pin for green - never explicitly referenced
 #define PIN_BLUE     11 // pin for blue - never explicitly referenced
 #define PIN_DATA     2 //Data pin for 433hz receiver
 
-#define ADDR_PROGRAM_SELECT 10000
-#define ADDR_COLOR_SELECT 10001
-#define ADDR_CONFIG 10002
+#define ADDR_PROGRAM 10000
+#define UNIT_PROGRAM_OFF 0
+#define UNIT_PROGRAM_STATIC_1 1
+#define UNIT_PROGRAM_STATIC_2 2
+#define UNIT_PROGRAM_STATIC_3 3
+#define UNIT_PROGRAM_RAINBOW 4
+#define UNIT_PROGRAM_FADE12 5
+#define UNIT_PROGRAM_FADE123 6
+#define UNIT_PROGRAM_FADE23 7
+#define UNIT_PROGRAM_FADE13 8
+#define UNIT_PROGRAM_FLASH_RED 9
+#define UNIT_PROGRAM_FLASH_BLUE 10
+#define UNIT_PROGRAM_FLASH_GREEN 11
+#define UNIT_PROGRAM_FLASH_WHITE 12
 
-#define SET_RED_1 0
-#define SET_GREEN_1 1
-#define SET_BLUE_1 2
-#define SET_RED_1_MULTIPLIER 3
-#define SET_GREEN_1_MULTIPLIER 4
-#define SET_BLUE_1_MULTIPLIER 5
+#define ADDR_STATIC_1 10011
+#define ADDR_STATIC_2 10012
+#define ADDR_STATIC_3 10013
+#define UNIT_STATIC_DARK_RED 0
+#define UNIT_STATIC_RED 1
+#define UNIT_STATIC_PINK 2
+#define UNIT_STATIC_PURPLE 3
+#define UNIT_STATIC_DARK_BLUE 4
+#define UNIT_STATIC_BLUE 5
+#define UNIT_STATIC_LIGHT_BLUE 6
+#define UNIT_STATIC_LIGHT_GREEN 7
+#define UNIT_STATIC_GREEN 8
+#define UNIT_STATIC_DARK_GREEN 9
+#define UNIT_STATIC_YELLOW 10
+#define UNIT_STATIC_ORANGE 11
+#define UNIT_STATIC_DARK_ORANGE 12
+#define UNIT_STATIC_BRIGHT_WHITE 13
+#define UNIT_STATIC_WARM_WHITE 14
 
-#define SET_RED_2 6
-#define SET_GREEN_2 7
-#define SET_BLUE_2 8
-#define SET_RED_2_MULTIPLIER 9
-#define SET_GREEN_2_MULTIPLIER 10
-#define SET_BLUE_2_MULTIPLIER 11
+struct RGB {
+  byte r;
+  byte g;
+  byte b;
+};
 
-#define APPLY 12
+RGB currentRgb = {0, 0, 0};
+RGB targetRgb = {0, 0, 0};
 
-#define SET_FADE_SPEED 0
-#define SET_RAINBOW_SPEED 1
-#define SET_FADE_TIMES 2
-#define SET_FADE_TIMES_MULTIPLIER 3
+RGB color1 = {0, 0, 0};
+RGB color2 = {0, 0, 0};
+RGB color3 = {0, 0, 0};
 
-#define PROG_OFF 0
-#define PROG_STATIC_1 1
-#define PROG_STATIC_2 2
-#define PROG_RAINBOW 3
-#define PROG_PULSE 4
-#define PROG_FLASH_RED 5
+RGB c_off = {0, 0, 0};
+RGB c_darkred = {128, 0, 0};
+RGB c_red = {255, 0, 0};
+RGB c_pink = {255, 32, 8};
+RGB c_purple = {1, 1, 1};
+RGB c_darkblue = {1, 1, 1};
+RGB c_blue = {1, 1, 1};
+RGB c_lightblue = {1, 1, 1};
+RGB c_lightgreen = {1, 1, 1};
+RGB c_green = {1, 1, 1};
+RGB c_darkgreen = {1, 1, 1};
+RGB c_yellow = {1, 1, 1};
+RGB c_orange = {1, 1, 1};
+RGB c_darkorange = {1, 1, 1};
+RGB c_brightwhite = {1, 1, 1};
+RGB c_warmwhite = {1, 1, 1};
 
-long current_rgb[3];
-long target_rgb[3];
-long begin_rgb[3];
-
-long rgb_1[3];
-long rgb_2[3];
-long rgb_r_1[3];
-long rgb_r_2[3];
-long rgb_r_1_m[3];
-long rgb_r_2_m[3];
-
-
-long b_rgb_1[3];
-long b_rgb_2[3];
-long b_rgb_r_1[3];
-long b_rgb_r_2[3];
-long b_rgb_r_1_m[3];
-long b_rgb_r_2_m[3];
-
-long rgbval;
-float hue=0.0000, saturation=1, value=1;
-#define HUE_MAX  6000.0
-
-int stepSpeed = 17;
-int animationcounter = 0;
-int animationlength = 120;
-int HUE_DELTA = 5;
-
-int flash_max = 3;
-int flash_counter = flash_max;
-int program_back = PROG_RAINBOW;
-int program = PROG_RAINBOW;
-int animationlength_back = animationlength;
-
-long k;
+void writeRgb(RGB values) {
+  analogWrite(PIN_RED, values.r);
+  analogWrite(PIN_GREEN, values.g);
+  analogWrite(PIN_BLUE, values.b);
+}
 
 void setup () {
-  for (k=0; k<3; k++) {
-    pinMode(PIN_RED + k, OUTPUT);
-    rgb_1[k]=0;
-    rgb_2[k]=0;
-    rgb_r_1[k]=0;
-    rgb_r_2[k]=0;
-    rgb_r_1_m[k]=0;
-    rgb_r_2_m[k]=0;
-    current_rgb[k]=0;
-    target_rgb[k]=0;
-    analogWrite(PIN_RED + k, current_rgb[k]);
-  }
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
+  writeRgb(c_pink);
 
   Serial.begin(115200);
-  
   // Initialize receiver on interrupt 0 (= digital pin 2), calls the callback "showCode"
   // after 3 identical codes have been received in a row. (thus, keep the button pressed
   // for a moment)
@@ -149,10 +141,9 @@ void setup () {
   // to connect the receiver.
   NewRemoteReceiver::init(0, PIN_DATA, showCode);
 }
-//long bright[3] = { 256, 128, 90};
-bool pulse = 0;
+
 void loop() {
-  switch (program) {
+  /*switch (program) {
       case PROG_RAINBOW:
           hue += HUE_DELTA;
           if (hue > HUE_MAX) {
@@ -257,23 +248,46 @@ void loop() {
   setRed(current_rgb[0]);
   setGreen(current_rgb[1]);
   setBlue(current_rgb[2]);
-  animationcounter++;
-}
-
-void setRed(int color) {
-  analogWrite(PIN_RED, color);
-}
-
-void setGreen(int color) {
-  analogWrite(PIN_GREEN, color);
-}
-
-void setBlue(int color) {
-  analogWrite(PIN_BLUE, color);
+  animationcounter++;*/
 }
 
 int easeInOutCubic(int t, int b, int c, int d) {
     return -c/2 * (cos(PI*t/d) - 1) + b;
+}
+
+RGB getColorFromUnit(int unit) {
+  switch (unit) {
+    case UNIT_STATIC_DARK_RED:
+      return c_darkred;
+    case UNIT_STATIC_RED:
+      return c_red;
+    case UNIT_STATIC_PINK:
+      return c_pink;
+    case UNIT_STATIC_PURPLE:
+      return c_purple;
+    case UNIT_STATIC_DARK_BLUE:
+      return c_darkblue;
+    case UNIT_STATIC_BLUE:
+      return c_blue;
+    case UNIT_STATIC_LIGHT_BLUE:
+      return c_lightblue;
+    case UNIT_STATIC_LIGHT_GREEN:
+      return c_lightgreen;
+    case UNIT_STATIC_GREEN:
+      return c_green;
+    case UNIT_STATIC_DARK_GREEN:
+      return c_darkgreen;
+    case UNIT_STATIC_YELLOW:
+      return c_yellow;
+    case UNIT_STATIC_ORANGE:
+      return c_orange;
+    case UNIT_STATIC_DARK_ORANGE:
+      return c_darkorange;
+    case UNIT_STATIC_BRIGHT_WHITE:
+      return c_brightwhite;
+    case UNIT_STATIC_WARM_WHITE:
+      return c_warmwhite;
+  }
 }
 
 int level = 0;
@@ -282,116 +296,6 @@ void showCode(NewRemoteCode receivedCode) {
   level = 0;
   if (receivedCode.dimLevelPresent) {
     level = receivedCode.dimLevel;
-  }
-  switch (receivedCode.address) {
-    case ADDR_PROGRAM_SELECT:
-        switch (receivedCode.unit) {
-          case PROG_OFF:
-          case PROG_STATIC_1:
-          case PROG_STATIC_2:
-          case PROG_RAINBOW:
-          case PROG_PULSE:
-              program = receivedCode.unit;
-              hue = 0;
-              animationcounter = 0;
-              begin_rgb[0] = current_rgb[0];
-              begin_rgb[1] = current_rgb[1];
-              begin_rgb[2] = current_rgb[2];
-              rgb_1[0] = rgb_r_1[0] + rgb_r_1_m[0];
-              rgb_1[1] = rgb_r_1[1] + rgb_r_1_m[1];
-              rgb_1[2] = rgb_r_1[2] + rgb_r_1_m[2];
-              rgb_2[0] = rgb_r_2[0] + rgb_r_2_m[0];
-              rgb_2[1] = rgb_r_2[1] + rgb_r_2_m[1];
-              rgb_2[2] = rgb_r_2[2] + rgb_r_2_m[2];
-              break;
-          case PROG_FLASH_RED:
-              program_back = program;
-              program = receivedCode.unit;
-              animationlength = 10;
-              for (k = 0; k<3; k++) {
-                b_rgb_1[k] = rgb_1[k];
-                b_rgb_2[k] = rgb_2[k];
-                b_rgb_r_1[k] = rgb_r_1[k];
-                b_rgb_r_2[k] = rgb_r_2[k];
-                b_rgb_r_1_m[k] = rgb_r_1_m[k];
-                b_rgb_r_2_m[k] = rgb_r_2_m[k];
-              }
-              animationcounter = 0;
-              flash_counter = 0;
-
-              rgb_1[0] = 255;
-              rgb_1[1] = 0;
-              rgb_1[2] = 0;
-              
-              current_rgb[0] = rgb_1[0];
-              current_rgb[1] = rgb_1[1];
-              current_rgb[2] = rgb_1[2];
-
-              rgb_2[0] = 0;
-              rgb_2[1] = 0;
-              rgb_2[2] = 0;
-            
-              break;
-        }
-        break;
-    case ADDR_COLOR_SELECT:
-        switch (receivedCode.unit) {
-          case SET_RED_1:
-              rgb_r_1[0] = level;
-              break;
-          case SET_GREEN_1:
-              rgb_r_1[1] = level;
-              break;
-          case SET_BLUE_1:
-              rgb_r_1[2] = level;
-              break;
-          case SET_RED_1_MULTIPLIER:
-              rgb_r_1_m[0] = level*16;
-              break;
-          case SET_GREEN_1_MULTIPLIER:
-              rgb_r_1_m[1] = level*16;
-              break;
-          case SET_BLUE_1_MULTIPLIER:
-              rgb_r_1_m[2] = level*16;
-              break;
-
-          case SET_RED_2:
-              rgb_r_2[0] = level;
-              break;
-          case SET_GREEN_2:
-              rgb_r_2[1] = level;
-              break;
-          case SET_BLUE_2:
-              rgb_r_2[2] = level;
-              break;
-          case SET_RED_2_MULTIPLIER:
-              rgb_r_2_m[0] = level*16;
-              break;
-          case SET_GREEN_2_MULTIPLIER:
-              rgb_r_2_m[1] = level*16;
-              break;
-          case SET_BLUE_2_MULTIPLIER:
-              rgb_r_2_m[2] = level*16;
-              break;
-        }
-        break;
-    case ADDR_CONFIG:
-        switch (receivedCode.unit) {
-          case SET_FADE_SPEED:
-              animationlength = level * 15; // in quarter seconds
-              break;
-          case SET_RAINBOW_SPEED:
-              HUE_DELTA = level;
-              break;
-          case SET_FADE_TIMES:
-              //fadeTimes = level;
-              break;
-          case SET_FADE_TIMES_MULTIPLIER:
-              //fadeTimes *= level;
-              break;
-      
-        }
-        break;
   }
 
   // Print the received code.
@@ -426,43 +330,32 @@ void showCode(NewRemoteCode receivedCode) {
   Serial.print(", period: ");
   Serial.print(receivedCode.period);
   Serial.println("us.");
+
+  switch (receivedCode.address) {
+    case ADDR_PROGRAM:
+      switch (receivedCode.unit) {
+        case UNIT_PROGRAM_OFF:
+          writeRgb(c_off);
+          break;
+        case UNIT_PROGRAM_STATIC_1:
+          writeRgb(color1);
+          break;
+        case UNIT_PROGRAM_STATIC_2:
+          writeRgb(color2);
+          break;
+        case UNIT_PROGRAM_STATIC_3:
+          writeRgb(color3);
+          break;
+      }
+      break;
+    case ADDR_STATIC_1:
+      color1 = getColorFromUnit(receivedCode.unit);
+      break;
+    case ADDR_STATIC_2:
+      color2 = getColorFromUnit(receivedCode.unit);
+      break;
+    case ADDR_STATIC_3:
+      color3 = getColorFromUnit(receivedCode.unit);
+      break;
+  }
 }
-
-long HSV_to_RGB( float h, float s, float v ) {
-  /* modified from Alvy Ray Smith's site: http://www.alvyray.com/Papers/hsv2rgb.htm */
-  // H is given on [0, 6]. S and V are given on [0, 1].
-  // RGB is returned as a 24-bit long #rrggbb
-  int i;
-  float m, n, f;
-
-  // not very elegant way of dealing with out of range: return black
-  if ((s<0.0) || (s>1.0) || (v<1.0) || (v>1.0)) {
-    return 0L;
-  }
-
-  if ((h < 0.0) || (h > 6.0)) {
-    return long( v * 255 ) + long( v * 255 ) * 256 + long( v * 255 ) * 65536;
-  }
-  i = floor(h);
-  f = h - i;
-  if ( !(i&1) ) {
-    f = 1 - f; // if i is even
-  }
-  m = v * (1 - s);
-  n = v * (1 - s * f);
-  switch (i) {
-  case 6:
-  case 0: 
-    return long(v * 255 ) * 65536 + long( n * 255 ) * 256 + long( m * 255);
-  case 1: 
-    return long(n * 255 ) * 65536 + long( v * 255 ) * 256 + long( m * 255);
-  case 2: 
-    return long(m * 255 ) * 65536 + long( v * 255 ) * 256 + long( n * 255);
-  case 3: 
-    return long(m * 255 ) * 65536 + long( n * 255 ) * 256 + long( v * 255);
-  case 4: 
-    return long(n * 255 ) * 65536 + long( m * 255 ) * 256 + long( v * 255);
-  case 5: 
-    return long(v * 255 ) * 65536 + long( m * 255 ) * 256 + long( n * 255);
-  }
-} 
